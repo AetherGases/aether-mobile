55a1e365882ee044c137fdea17e41e933e653bb6
IsaDaHxra <isabellyhora-ieg@instituto.ad>
2026-08-04 09:49:20 -0300

    feat: add colors variables

--- app/src/main/java/com/aether/application/core/ui/theme/Color.kt
+++ app/src/main/java/com/aether/application/core/ui/theme/Color.kt
@@ -1,2 +1,47 @@
-#!/usr/bin/env kotlin
+package com.aether.core.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+// Cartela roxa
+
+val purple900 = Color(0x1E0A4A);
+val purple700 = Color(0x5B21B6);
+val purple500 = Color(0x7C3AED);
+val purple300 = Color(0xC4B5FD);
+val purple100 = Color(0xEDE9FE);
+
+// Cartela verde
+
+val green900 = Color(0x0D3B35);
+val green700 = Color(0x1A5C4A);
+val green500 = Color(0x2DBD7E);
+val green300 = Color(0x5DDBA5);
+val green100 = Color(0xA8F0D2);
+
+// Cores de texto - Modo claro
+
+val textPrimaryLight = Color(0x1E0A4A);
+val textSecondaryLight = Color(0x4A435E);
+val textTertiaryLight = Color(0x7E8294);
+val textDisabledLight = Color(0xB3B7C9);
+
+// Cores de texto - Modo escuro
+
+val textPrimaryDark = Color(0xF3F7F5);
+val textSecondaryDark = Color(0xE8ECF8);
+val textTertiaryDark = Color(0x98A2C7);
+val textDisabled = Color(0x70789A);
+
+// Fundos
+
+val backgroundDark = Color(0x0B101E);
+val backgroundDarkElevated = (0x161C2D);
+val backgroundLight = Color(0xF0F4F8);
+val backgroundLightElevated = Color(0xFFFFFF);
+
+// Cores semânticas
+
+val darkRed = Color(0xBE182C);
+val lightRed = Color(0xE93D53);
+val darkYellow = Color(0xF5A524);
+val lightYellow = Color(0xFFCD4D);
