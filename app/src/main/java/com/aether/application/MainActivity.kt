69d7ea4c17c18bd7723f2981b0bbaf1b7af6f0e9
Vini-Boas <vinicius.vboas1@gmail.com>
2026-09-07 20:04:57 -0300

    feat: add compose navigation for login screen

--- app/src/main/java/com/aether/application/MainActivity.kt
+++ app/src/main/java/com/aether/application/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.aether.application
 
+import com.aether.application.core.navigation.AppNavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.aether.application.feature.auth.presentation.screen.LoginScreen
-import com.aether.application.feature.auth.presentation.viewmodel.LoginViewModel
 import com.aether.core.ui.theme.AetherTheme
-import org.koin.compose.viewmodel.koinViewModel
 
 
 class MainActivity : ComponentActivity() {
@@ -24,33 +16,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AetherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val viewModel = koinViewModel<LoginViewModel>()
-                    val uiState by viewModel.uiState.collectAsState()
-
-                    LoginScreen(
-                        onLoginClick = { email, password, rememberMe ->
-                            viewModel.onLoginClick(email, password, rememberMe)
-                        },
-                        onForgotPasswordClick = {},
-                        isLoading = uiState.isLoading,
-                        errorMessage = uiState.errorMessage,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation(modifier = Modifier.fillMaxSize())
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun LoginScreenPreview() {
-    AetherTheme {
-        LoginScreen(
-            onLoginClick = { _, _, _ -> },
-            onForgotPasswordClick = {}
-        )
     }
 }
