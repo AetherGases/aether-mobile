6ae167b220ed88e77bd491dc4724b2da7fab6598
Vini-Boas <vinicius.vboas1@gmail.com>
2026-09-24 11:43:10 -0300

    fix: add seriazable to ResetPasswordSendCodeRequest

--- app/src/main/java/com/aether/application/feature/auth/data/remote/dto/ResetPasswordSendCodeRequest.kt
+++ app/src/main/java/com/aether/application/feature/auth/data/remote/dto/ResetPasswordSendCodeRequest.kt
@@ -1,5 +1,8 @@
 package com.aether.application.feature.auth.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ResetPasswordSendCodeRequest(
     val email: String
 )
