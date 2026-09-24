7256dfa5705b5473709d9e106aace58fa8fa6ac5
Vini-Boas <vinicius.vboas1@gmail.com>
2026-09-24 07:37:05 -0300

    feat(app): restore persisted session on app startup

--- app/src/main/java/com/aether/application/AetherApplication.kt
+++ app/src/main/java/com/aether/application/AetherApplication.kt
@@ -1,19 +1,33 @@
 package com.aether.application
 
 import android.app.Application
+import com.aether.application.core.auth.storage.SessionManager
 import com.aether.application.core.di.AppModule
 import com.aether.application.core.di.NetworkModule
 import com.aether.application.core.di.dataStoreModule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import org.koin.core.context.startKoin
 
-class AetherApplication : Application() {
+class AetherApplication : Application(), KoinComponent {
+
+    private val sessionManager: SessionManager by inject()
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
             androidContext(this@AetherApplication)
             modules(AppModule.module, NetworkModule.module, dataStoreModule)
+        }
+
+        applicationScope.launch {
+            sessionManager.restoreSession()
         }
     }
 }
