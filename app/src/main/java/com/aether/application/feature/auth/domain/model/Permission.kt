7ab7bb403c8e56d6a82685569e505880f00f69ca
Vini-Boas <vinicius.vboas1@gmail.com>
2026-09-24 07:38:40 -0300

    fix(auth): make Permission a proper serializable model

---
+++ app/src/main/java/com/aether/application/feature/auth/domain/model/Permission.kt
@@ -0,0 +1,10 @@
+package com.aether.application.feature.auth.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Permission(
+    val id: Int,
+    val name: String,
+    val description: String? = null
+)
